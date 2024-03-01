26d50895ae6bf774e338383c5d9d8da8820995ec
aunnieasd <134691742+aunnieasd@users.noreply.github.com>
2024-03-01 20:42:18 +0700

    Add Timeslot Schema

--- models/TimeSlot.js
+++ models/TimeSlot.js
@@ -1,1 +1,27 @@
 const mongoose = requie('mongoose') ;
+
+const TimeSlotSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: [true , "Please provide date"]
+    },
+    startTime: {
+        type: Time,
+        required: [true , "Please provide Start Time"]
+    },
+    endTime: {
+        type: Time,
+        required: [true , "Please provide End Time"]
+    },
+    capacity: {
+        type: Number,
+        required: [true , "Please provide capacity"]
+    },
+    participants: {
+        type: [{uid:mongoose.Schema.ObjectId}],
+        required: [true , "Please provide participants"]
+    },
+    description: {
+        type: String
+    }
+})
