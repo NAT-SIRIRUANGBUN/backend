e0b2278875ce58d33a3a6ad71522471a6906ce6d
TenJit <tenjitrattanakul@gmail.com>
2024-03-02 12:01:50 +0700

    pagination and query

--- controller/companies.js
+++ controller/companies.js
@@ -4,13 +4,57 @@ const Company = require('../models/Company');
 //@route GET /api/v1/companies
 //@access Public
 exports.getCompanies = async (req,res,next) => {
-    try {
-        const companies = await Company.find() ;
 
-        res.status(200).json({success: true, count: companies.length , data: companies});
-    } catch(err){
-        console.error(err);
-        res.status(400).json({success: false});
+    let query;
+
+    const reqQuery={...req.query};
+
+    const removeFields=['select','sort','page','limit'];
+
+    removeFields.forEach(param=>delete reqQuery[param]);
+    console.log(reqQuery);
+
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=> `$${match}`);
+    query = Company.find(JSON.parse(queryStr));
+
+    if(req.query.select){
+        const fields=req.query.select.split(',').join(' ');
+        query=query.select(fields);
+    }
+
+    if(req.query.sort){
+        const sortBy=req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    }else{
+        query = query.sort('name');
+    }
+
+    const page = parseInt(req.query.page,10)||1;
+    const limit = parseInt(req.query.limit,10)||25;
+    const startIndex=(page-1)*limit;
+    const endIndex = page*limit;
+    
+    try{
+        const total = await Company.countDocuments();
+        query = query.skip(startIndex).limit(limit);
+        const companies = await query;
+        const pagination={};
+        if(endIndex < total){
+            pagination.next={
+                page:page+1,
+                limit
+            }
+        }
+        if(startIndex > 0){
+            pagination.prev={
+                page:page-1,
+                limit
+            }
+        }
+        res.status(200).json({success:true, count:companies.length, data:companies});
+    }catch(err){
+        res.status(400).json({success:false});
     }
     
 };
