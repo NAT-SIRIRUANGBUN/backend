f6858f27e76ec911b225c90acd0aa6b8efd207f3
TenJit <tenjitrattanakul@gmail.com>
2024-03-02 21:52:23 +0700

    Add pagination and query to timeslot

--- controller/timeslot.js
+++ controller/timeslot.js
@@ -1,16 +1,61 @@
 const {TimeSlot} = require('../models/Company')
 
 exports.getTimeslots = async (req , res , next) => {
-    try {
-        const timeslot = await TimeSlot.find().populate({
-            path: 'company',
-            select: 'name tel contact_email'
-        });
-        res.status(200).json({success : true , timeslot})
+    let query;
+
+    const reqQuery={...req.query};
+
+    const removeFields=['select','sort','page','limit'];
+
+    removeFields.forEach(param=>delete reqQuery[param]);
+
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=> `$${match}`);
+    query = TimeSlot.find(JSON.parse(queryStr));
+
+    if(req.query.select){
+        const fields=req.query.select.split(',').join(' ');
+        query=query.select(fields);
     }
-    catch(err) {
-        console.error(err)
-        res.status(400).json({success : false , msg : "Something Wrong"})
+
+    if(req.query.sort){
+        const sortBy=req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    }else{
+        query = query.sort('date');
+    }
+
+    query = query.populate({
+        path: 'company',
+        select: 'name tel contact_email'
+    });
+
+    const page = parseInt(req.query.page,10)||1;
+    const limit = parseInt(req.query.limit,10)||10;
+    const startIndex=(page-1)*limit;
+    const endIndex = page*limit;
+    
+    try{
+        const total = await TimeSlot.countDocuments();
+        query = query.skip(startIndex).limit(limit);
+        const timeslots = await query;
+        const pagination={};
+        if(endIndex < total){
+            pagination.next={
+                page:page+1,
+                limit
+            }
+        }
+        if(startIndex > 0){
+            pagination.prev={
+                page:page-1,
+                limit
+            }
+        }
+        res.status(200).json({success:true, count:timeslots.length, data:timeslots});
+    }catch(err){
+        console.error(err);
+        res.status(400).json({success:false , msg : 'Something Wrong'});
     }
 }
 
