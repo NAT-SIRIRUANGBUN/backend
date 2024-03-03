99824a411cb1efce33b6a6d05ba3062b2184eaed
aunnieasd <134691742+aunnieasd@users.noreply.github.com>
2024-03-03 22:42:34 +0700

    protect company logout

--- routes/companies.js
+++ routes/companies.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route('/').get(getCompanies).post(protect , authorize('admin') , createCompany);
 router.route('/:id').get(getCompany).put(protect , authorize('admin','company') , updateCompany).delete(protect , authorize('admin','company'), deleteCompany);
 router.route('/auth/login').post(login_company)
-router.route('/auth/logout').get(logout)
+router.route('/auth/logout').get(protect , authorize('company') , logout)
 router.route('/auth/getdetail').get(protect , authorize('company') , getCompanyDetail)
 router.route('/:id/timeslot').get(getCompanyTimeSlot).post(protect , authorize('admin','company') , createTimeslot)
 router.route('/:id/timeslot/:timeslotid').put(protect , authorize('admin','company') , updateTimeslot).delete(protect , authorize('admin','company') , deleteTimeslot)
